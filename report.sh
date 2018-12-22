97120ae63b13691c3243efe2e6ac1653ab4298b9
leunammejii <theemailaddressishare@gmail.com>
2018-12-22 02:12:28 -0500

    Output CSV format to stdout

--- report.sh
+++ report.sh
@@ -4,66 +4,115 @@
 # Usage      : bash report.sh DIRECTORY
 #
 
-for x in $(ls "$1" | grep -vE ".placeholder|^\.$|^\.\.$")
+
+echo "DateFound,ReferenceLink,ThreatActorEmail,EmailType,KitMailer,Target,PhishingDomain,KitName,ThreatActor,KitHash,KitUrl"
+
+for x in $(ls "$@" | grep -vE ".placeholder|^\.$|^\.\.$|md5sum\.report")
 do
     (
-        echo -e "\n########################################################\n"
+        cd "$1/$x"
 
-        echo "$x"
-
-        cd ./*/"$x"
-
-        find -name *.zip -exec unzip -o {} \;
-        find -name *.rar -exec unrar e {} \;
+        find -name *.rar -exec unrar e {} \; > /dev/null
+        find -name *.zip -exec unzip -qo {} \;
 
         touch .inv
 
-        echo -e "DateFound" > pkt.report
-        date +%m/%d/%Y >> pkt.report
+        echo -e "\n########################################################\n" > pkt.report
+
+        echo -e "DateFound" >> pkt.report
+        DateFound=$(date +%m/%d/%Y)
+        echo "$DateFound" >> pkt.report
 
         echo -e "\nReferenceLink" >> pkt.report
-        echo - >> pkt.report
-
-        echo -e "\nEmailType" >> pkt.report
-        grep -rhoE "\s+=\s+('|\")\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b('|\")" | \
-            tr -d " =" | \
-            grep \@ | \
-            awk -F "@" '{print $2}' | \
-            awk -F "." '{print $1}' | \
-            grep -v example | \
-            sort -u >> pkt.report
-
-        echo -e "\nKitMailer" >> pkt.report
-        echo - >> pkt.report
-
-        echo -e "\nTarget" >> pkt.report
-        echo - >> pkt.report
-
-        echo -e "\nPhishingDomain" >> pkt.report
-        echo "$x" | \
-            sed "s/^\./hxxps:\/\//gi" >> pkt.report
-
-        echo -e "\nKitName" >> pkt.report
-        find . -name *.zip -exec md5sum {} \; | \
-            awk -F "  " '{print $2}' | \
-            awk -F "/" '{print $2}' | \
-            sort -u >> pkt.report
-
-        echo -e "\nThreatActor" >> pkt.report
-        echo - >> pkt.report
-
-        echo -e "\nKitHash" >> pkt.report
-        find -name *.zip -exec md5sum {} \; | \
-            awk -F "  " '{print $1}' | \
-            sort -u >> pkt.report
+        ReferenceLink="-"
+        echo "$ReferenceLink" >> pkt.report
 
         echo -e "\nThreatActorEmail" >> pkt.report
-        grep -rhoE "\s+=\s+('|\")\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b('|\")" | \
-            tr -d " ='\"" | \
-            grep \@ | \
-            grep -v example.com | \
-            sort -u >> pkt.report
+        ThreatActorEmail=$(
+            grep -rhoE "\s+=\s+('|\")\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b('|\")" KitJackinSeason/.secureapple-appleid.servehttp.com | \
+                tr -d " ='\"" | \
+                grep \@ | \
+                grep -v example.com | \
+                sort -u | \
+                paste -sd "|" -
+        )
+        echo "$ThreatActorEmail" >> pkt.report
+            
+
+        echo -e "\nEmailType" >> pkt.report
+        EmailType=$(
+            grep -rhoE "\s+=\s+('|\")\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b('|\")" | \
+                tr -d " =" | \
+                grep \@ | \
+                awk -F "@" '{print $2}' | \
+                awk -F "." '{print $1}' | \
+                grep -v example | \
+                sort -u | \
+                paste -sd "|" -
+        )
+        echo $EmailType >> pkt.report
+        
+
+        echo -e "\nKitMailer" >> pkt.report
+        KitMailer="-"
+        echo "$KitMailer" >> pkt.report
+
+        echo -e "\nTarget" >> pkt.report
+        Target="-"
+        echo "$Target" >> pkt.report
+
+        echo -e "\nPhishingDomain" >> pkt.report
+        PhishingDomain="$x"
+        echo "$PhishingDomain" >> pkt.report
+
+        echo -e "\nKitName" >> pkt.report
+        KitName=$(
+            find . -name *.zip -exec md5sum {} \; | \
+                awk -F "  " '{print $2}' | \
+                awk -F "/" '{print $2}' | \
+                sort -u | \
+                paste -sd "|" -
+        )
+        echo $KitName >> pkt.report
+
+        echo -e "\nThreatActor" >> pkt.report
+        ThreatActor=$(
+            grep -rhioE "(created by .+|hacked by .+|coded by .+|edited by .+|signed by .+|made by .+)([^\r|\n|\=|\+|\"|\'|\,]+)\s+([\,\=\+\"\']|\-\-)" | \
+                sort -u | \
+                paste -sd "|" -
+        )
+        echo "$ThreatActor" >> pkt.report
+
+        echo -e "\nKitHash" >> pkt.report
+        KitHash=$(
+            find -name *.zip -exec md5sum {} \; | \
+                awk -F "  " '{print $1}' | \
+                sort -u | \
+                paste -sd "|" -
+        )
+        echo "$KitHash" >> pkt.report
+
+        echo -e "\nKitUrl" >> pkt.report
+        KitUrl="hxxps://$x/$(ls *.zip)"
+        echo "$KitUrl" >> pkt.report
+
+        echo "$DateFound,$ReferenceLink,$ThreatActorEmail,$EmailType,$KitMailer,$Target,$PhishingDomain,$KitName,$ThreatActor,$KitHash,$KitUrl"
 
         mv ../"${PWD##*/}" ../."${PWD##*/}"
+
+        # ABUSE_EMAIL_RECIPIENT=$(
+        #     echo "$x" | \
+        #         rev | \
+        #         cut -d . -f 1-2 | \
+        #         rev | \
+        #         xargs whois | \
+        #         grep -i abuse | \
+        #         grep -i email | \
+        #         tail -n 1 | \
+        #         awk -F ": " '{print $2}'
+        # )
     )
 done
+
+find -name *.rar -exec md5sum {} \; | sort > "$1/md5sum.report"
+find -name *.zip -exec md5sum {} \; | sort >> "$1/md5sum.report"
